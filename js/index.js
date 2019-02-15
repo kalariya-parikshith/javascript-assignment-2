a490d8b0746295a1402cd72b124af976dfe35def
kalariya-parikshith <k.parikshith93@gmail.com>
2019-02-15 11:20:32 +0530

    item search bug fixed

--- js/index.js
+++ js/index.js
@@ -101,32 +101,6 @@ displayBillAndNumberOfItemsOnTable = function(tableNumber, totalBill, totalItems
 	document.getElementById(tableNumber).innerHTML = "Table "+ (tableNumber) + "<br>Rs. "+totalBill+" | Total items: "+totalItems;
 }
 
-document.addEventListener("dragstart", function(event) {
-	event.dataTransfer.setData("itemId",event.target.id);
-},false);
-
-document.addEventListener("dragover", function(event) {
-	event.preventDefault();
-});
-
-document.addEventListener("drop", function(event) {
-	event.preventDefault();
-	var tableNumber = event.target.id;
-	var itemId = event.dataTransfer.getData("itemId");
-
-	if(isItemPresentOnTable(tableNumber, itemId)) {
-		incrementItemServingOnTable(tableNumber, itemId);
-	}
-	else {
-		tableOrders[parseInt(tableNumber)].push(itemId+","+"1");
-	}
-
-	var totalBill = getTotalBillOfATable(tableNumber);
-	var totalItems = getTotalNumberOfItemsOnTable(tableNumber);
-	displayBillAndNumberOfItemsOnTable(tableNumber, totalBill, totalItems);
-},false);
-
-
 addHeaderForOrderDetails = function() {
 	var tableRow = document.createElement('TR');
 	tableRow.id = 'tableHead';
@@ -152,6 +126,7 @@ addHeaderForOrderDetails = function() {
 	removeItem.innerHTML="remove";
 	document.getElementById('tableHead').appendChild(removeItem);
 }
+
 removeItemOnTable = function(tableNumber, NthItem) {
 	tableOrders[tableNumber].splice(NthItem,1);
 	displayTableDetails(tableNumber);
@@ -171,41 +146,41 @@ updateServing = function(tableNumber, NthItem) {
 
 detailsOfItemsOnTable = function(tableNumber, NthItem) {
 	var itemDetailsOnTable = document.createElement('TR');
-    itemDetailsOnTable.id = "itemOnTable"+NthItem;
-    document.getElementById('orderDetailsTable').appendChild(itemDetailsOnTable);
+	itemDetailsOnTable.id = "itemOnTable"+NthItem;
+	document.getElementById('orderDetailsTable').appendChild(itemDetailsOnTable);
 
-    var serialNumberOnTable = document.createElement('TD');
-    serialNumberOnTable.innerHTML = NthItem+1;
-    document.getElementById('itemOnTable'+NthItem).appendChild(serialNumberOnTable);
+	var serialNumberOnTable = document.createElement('TD');
+	serialNumberOnTable.innerHTML = NthItem+1;
+	document.getElementById('itemOnTable'+NthItem).appendChild(serialNumberOnTable);
 
-    var itemNameOnTable = document.createElement('TD');
-    itemNameOnTable.innerHTML = getNthItemNameOnTable(tableNumber, NthItem);
-    document.getElementById('itemOnTable'+NthItem).appendChild(itemNameOnTable);
+	var itemNameOnTable = document.createElement('TD');
+	itemNameOnTable.innerHTML = getNthItemNameOnTable(tableNumber, NthItem);
+	document.getElementById('itemOnTable'+NthItem).appendChild(itemNameOnTable);
 
-    var priceOfItemOnTable = document.createElement('TD');
-    priceOfItemOnTable.innerHTML = getNthItemPriceOnTable(tableNumber, NthItem);
-    document.getElementById('itemOnTable'+NthItem).appendChild(priceOfItemOnTable);
+	var priceOfItemOnTable = document.createElement('TD');
+	priceOfItemOnTable.innerHTML = getNthItemPriceOnTable(tableNumber, NthItem);
+	document.getElementById('itemOnTable'+NthItem).appendChild(priceOfItemOnTable);
 
-    var itemServingOnTable = document.createElement('INPUT');
-    itemServingOnTable.type = "number";
-    itemServingOnTable.min = "1";
-    itemServingOnTable.value = getNthItemServingOnTable(tableNumber, NthItem);
-    itemServingOnTable.id = "itemServing"+NthItem;
-    itemServingOnTable.setAttribute('onchange', 'updateServing('+ tableNumber +','+ NthItem +')');
+	var itemServingOnTable = document.createElement('INPUT');
+	itemServingOnTable.type = "number";
+	itemServingOnTable.min = "1";
+	itemServingOnTable.value = getNthItemServingOnTable(tableNumber, NthItem);
+	itemServingOnTable.id = "itemServing"+NthItem;
+	itemServingOnTable.setAttribute('onchange', 'updateServing('+ tableNumber +','+ NthItem +')');
 
-    var itemServingOnTableTD = document.createElement('TD');
-    itemServingOnTableTD.appendChild(itemServingOnTable);
-    document.getElementById('itemOnTable'+NthItem).appendChild(itemServingOnTable);
+	var itemServingOnTableTD = document.createElement('TD');
+	itemServingOnTableTD.appendChild(itemServingOnTable);
+	document.getElementById('itemOnTable'+NthItem).appendChild(itemServingOnTable);
 
-    var removeIcon = document.createElement('i');
-    removeIcon.className = "material-icons";
-    removeIcon.innerHTML = "delete";
-    removeIcon.id = "remove"+NthItem;
-    removeIcon.setAttribute("onclick",'removeItemOnTable('+tableNumber+','+NthItem+')');
+	var removeIcon = document.createElement('i');
+	removeIcon.className = "material-icons";
+	removeIcon.innerHTML = "delete";
+	removeIcon.id = "remove"+NthItem;
+	removeIcon.setAttribute("onclick",'removeItemOnTable('+tableNumber+','+NthItem+')');
 
-    var removeIconTd = document.createElement('TD');
-    removeIconTd.appendChild(removeIcon);
-    document.getElementById('itemOnTable'+NthItem).appendChild(removeIconTd);
+	var removeIconTd = document.createElement('TD');
+	removeIconTd.appendChild(removeIcon);
+	document.getElementById('itemOnTable'+NthItem).appendChild(removeIconTd);
 }
 
 displayTableDetails = function(tableNumber) {
@@ -224,8 +199,33 @@ displayTableDetails = function(tableNumber) {
 	document.getElementById('orderDetailsDiv').style.display = 'block';
 }
 
+document.addEventListener("dragstart", function(event) {
+	event.dataTransfer.setData("itemId",event.target.id);
+},false);
+
+document.addEventListener("dragover", function(event) {
+	event.preventDefault();
+});
+
+document.addEventListener("drop", function(event) {
+	event.preventDefault();
+	var tableNumber = event.target.id;
+	var itemId = event.dataTransfer.getData("itemId");
+
+	if(isItemPresentOnTable(tableNumber, itemId)) {
+		incrementItemServingOnTable(tableNumber, itemId);
+	}
+	else {
+		tableOrders[parseInt(tableNumber)].push(itemId+","+"1");
+	}
+
+	var totalBill = getTotalBillOfATable(tableNumber);
+	var totalItems = getTotalNumberOfItemsOnTable(tableNumber);
+	displayBillAndNumberOfItemsOnTable(tableNumber, totalBill, totalItems);
+},false);
+
 document.getElementById('closeOrderDetailsDiv').addEventListener('click', function(){
-  document.getElementById('orderDetailsDiv').style.display = 'none';
+	document.getElementById('orderDetailsDiv').style.display = 'none';
 }, false);
 document.getElementById("generateBill").addEventListener('click', function(){
 	removeAllItemsOnTable(tableClicked);
@@ -234,51 +234,52 @@ document.getElementById("generateBill").addEventListener('click', function(){
 
 tablesOfDiv = document.getElementById("tablesDiv").getElementsByTagName('div');
 for(var i=0 ; i<tablesOfDiv.length; i++) {
-  tablesOfDiv[i].addEventListener('click', function(event){
-    tableClicked = event.target.id;
-    displayTableDetails(event.target.id);
-  },false);
+	tablesOfDiv[i].addEventListener('click', function(event){
+		tableClicked = event.target.id;
+		displayTableDetails(event.target.id);
+	},false);
 }
 
-var items = JSON.parse('{"items":[{"id":"1","name":"Veg biryani","rate":"210","course":"main veg"},{"id":"2","name":"Paneer biryani","rate":"250","course":"main veg"},{"id":"3","name":"Paneer butter masala","rate":"150","course":"main curry"},{"id":"4","name":"Chicken biryani","rate":"250","course":"main nonveg"},{"id":"5","name":"Mutton biryani","rate":"300","course":"main nonveg"},{"id":"6","name":"Papad","rate":"50","course":"starter"},{"id":"7","name":"Shahi tukda","rate":"80","course":"sweet desert"}]}');
+var items = JSON.parse('{"items":[{"id":"1","name":" Veg biryani","rate":"210","course":" main veg"},{"id":"2","name":" Paneer biryani","rate":"250","course":" main veg"},{"id":"3","name":" Paneer butter masala","rate":"150","course":" main curry"},{"id":"4","name":" Chicken biryani","rate":"250","course":" main nonveg"},{"id":"5","name":" Mutton biryani","rate":"300","course":" main nonveg"},{"id":"6","name":" Papad","rate":"50","course":" starter"},{"id":"7","name":" Shahi tukda","rate":"80","course":" sweet desert"}]}');
 
-function searchItem() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("searchItemBox");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("listOfItems");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
+searchItem = function() {
+	var input, filter, ul, li, a, i, txtValue;
+	input = document.getElementById("searchItemBox");
+	filter = " "+input.value.toUpperCase();
+	ul = document.getElementById("listOfItems");
+	li = ul.getElementsByTagName("li");
+	for (i = 0; i < li.length; i++) {
+		a = li[i];
+		txtValue = a.textContent || a.innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			li[i].style.display = "";
+		} else {
+			li[i].style.display = "none";
+		}
+	}
 }
+
 searchTable = function() {
-  var input = document.getElementById('searchTableBox').value.toUpperCase();
-  for(var i=1; i<=numberOfTables; i++) {
-    if(document.getElementById(i).innerHTML.toUpperCase().indexOf(input) > -1) {
-      document.getElementById(i).style.display = "";
-    } else {
-      document.getElementById(i).style.display = "none";
-    }
-  }
+	var input = document.getElementById('searchTableBox').value.toUpperCase();
+	for(var i=1; i<=numberOfTables; i++) {
+		if(document.getElementById(i).innerHTML.toUpperCase().indexOf(input) > -1) {
+			document.getElementById(i).style.display = "";
+		} else {
+			document.getElementById(i).style.display = "none";
+		}
+	}
 }
 
 items.items.forEach(function(obj) {
-  var li = document.createElement('LI');
-  li.id = obj.id;
-  li.draggable = true;
-  li.innerHTML = obj.name + '<br>' + obj.rate + '<br>' + '<span class="course">'+obj.course + '</span>';
-  document.getElementById('listOfItems').appendChild(li);
+	var li = document.createElement('LI');
+	li.id = obj.id;
+	li.draggable = true;
+	li.innerHTML = obj.name + '<br>' + obj.rate + '<br>' + '<span class="course">'+obj.course + '</span>';
+	document.getElementById('listOfItems').appendChild(li);
 });
 
 window.onclick = function(event) {
-  if (event.target == document.getElementById('orderDetailsDiv')) {
-    document.getElementById('orderDetailsDiv').style.display = "none";
-  }
+	if (event.target == document.getElementById('orderDetailsDiv')) {
+		document.getElementById('orderDetailsDiv').style.display = "none";
+	}
 }
